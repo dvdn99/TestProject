5f06f26ad9a0c571d5b5ac861b40a7df98cd3d1b
David <dvdn.99@protonmail.com>
2018-08-25 04:39:51 +0300

    Iniciando proyecto

---
+++ src/mipaquete/Main.java
@@ -0,0 +1,23 @@
+package mipaquete;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.print("Enter a number: ");
+		
+		num = sc.nextInt();
+		
+		System.out.println("Number is : "+num);
+		
+		sc.close();
+	}
+
+}
